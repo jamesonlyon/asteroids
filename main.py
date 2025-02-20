b19c8bd20892b85eba21d1affc23c45355196699
Jamey <jcalyon@users.noreply.github.com>
2025-02-20 11:45:34 -0800

    Add: Constants file and updated loading message

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    print(f"""Starting Asteroids!
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}""")
 
 if __name__ == "__main__":
     main()
