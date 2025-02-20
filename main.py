1a8a1a6e63aa2c6c91801b04fd23762c29567335
Jamey <jcalyon@users.noreply.github.com>
2025-02-20 12:46:25 -0800

    Added groups, finished CH.2

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # create the GUI screen
     fps = pygame.time.Clock() # create a new pygame.time.Clock object
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # create a player ojbect
     dt = 0 # create delta time (dt) variable set to 0
+
+    # Create player groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Add the Player class to both groups
+    Player.containers = (updateable, drawable)
+
+    # Create the player(s) objects after they're in the group
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # create a player ojbect
 
     while True: # Open the game loop
         for event in pygame.event.get(): # On window close, quit the game
@@ -19,9 +28,13 @@ def main():
                 print("Quitting Asteroids...")
                 return
         
+        updateable.update(dt) # update the updateable sprites
+
         screen.fill("black") # Fill the screen with black
-        player.draw(screen) # Draw the player sprite
-        player.update(dt)
+
+        for sprite in drawable: # draw the drawable sprites
+            sprite.draw(screen)
+            
         pygame.display.flip() # Refresh the screen
         
         dt = fps.tick(60) / 1000 # Limit framerate to 60 FPS
