500b8c37111c6564a0c8e02366de380b13188e87
Jamey <jcalyon@users.noreply.github.com>
2025-02-20 12:04:38 -0800

    Add fps limit at 60fps and tick to game loop

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ def main():
     # Set the GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # create a new pygame.time.Clock object
+    fps = pygame.time.Clock()
+    
+    # create delta time (dt) variable set to 0
+    dt = 0
+
     # Open the game loop
     while True:
 
@@ -18,11 +24,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill('black') # Fill the screen with black
+        screen.fill("black") # Fill the screen with black
         pygame.display.flip() # Refresh the screen
+
+        # Pause the game for 1/60th of a second and
+        # update the delta time with time passed since last frame
+        dt = fps.tick(60) / 1000
 
 
 # (For now) Don't touch below this line
-
 if __name__ == "__main__":
     main()
