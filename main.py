deda6b2a788172cf7380244054f38b26b26403c4
Jamey <jcalyon@users.noreply.github.com>
2025-02-20 11:55:50 -0800

    Add game loop and basic window with refresh

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import pygame
 from constants import *
 
+# Main Function
 def main():
-    print(f"""Starting Asteroids!
-Screen width: {SCREEN_WIDTH}
-Screen height: {SCREEN_HEIGHT}""")
+    pygame.init()
+    
+    # Print boot message
+    print(f"""Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}""")
+
+    # Set the GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Open the game loop
+    while True:
+
+        for event in pygame.event.get(): # On window close, quit the game
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill('black') # Fill the screen with black
+        pygame.display.flip() # Refresh the screen
+
+
+# (For now) Don't touch below this line
 
 if __name__ == "__main__":
     main()
