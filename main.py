2e4467f6b1c380ea967a5d1d3d0d278f4e5d66cd
Jamey <jcalyon@users.noreply.github.com>
2025-02-20 16:50:49 -0800

    Add: Asteroids and Asteroid Field

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField 
 
-# Main Function
 def main():
     pygame.init()
-
     print(f"""Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}""") # Print boot message
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # create the GUI screen
     fps = pygame.time.Clock() # create a new pygame.time.Clock object
-    dt = 0 # create delta time (dt) variable set to 0
 
-    # Create player groups
-    updateable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    # Add the Player class to both groups
-    Player.containers = (updateable, drawable)
-
-    # Create the player(s) objects after they're in the group
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # create a player ojbect
+
+    dt = 0 # create delta time (dt) variable set to 0
 
     while True: # Open the game loop
         for event in pygame.event.get(): # On window close, quit the game
@@ -28,18 +29,16 @@ def main():
                 print("Quitting Asteroids...")
                 return
         
-        updateable.update(dt) # update the updateable sprites
+        updatable.update(dt) # update the updatable sprites
 
         screen.fill("black") # Fill the screen with black
 
         for sprite in drawable: # draw the drawable sprites
             sprite.draw(screen)
-            
+
         pygame.display.flip() # Refresh the screen
         
         dt = fps.tick(60) / 1000 # Limit framerate to 60 FPS
 
-
-# Pygame Main Function Call
 if __name__ == "__main__":
     main()
